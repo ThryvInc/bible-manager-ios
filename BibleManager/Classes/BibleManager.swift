386a5d4fc9f625068ae5b01c06c6bef296d53314
Elliot <elliot.schrock@gmail.com>
2017-02-16 11:18:01 -0500

    make mgr init public

--- BibleManager/Classes/BibleManager.swift
+++ BibleManager/Classes/BibleManager.swift
@@ -11,6 +11,7 @@ import Foundation
 open class BibleManager {
     open static var bibleManager: BibleManager!
     
+    public init() {}
     open func books(completion: ((Array<Book>) -> Void)) {}
     open func numberOfChapters(of book: Book, completion: ((Int64) -> Void)) {}
     open func verses(of book: Book, chapter: Int64, completion: ((Array<Verse>) -> Void)) {}
